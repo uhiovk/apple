2eab1f685fb62a0e84f3a3486c888d43c25a0052
uHioVK <u@uhio.uk>
2026-03-27 08:42:36 +0800

    refactor: improve clarity

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::{File, OpenOptions, create_dir_all, remove_file};
 use std::io;
 use std::path::{Path, PathBuf};
 
-use anyhow::{Error, Result, anyhow, bail, ensure};
+use anyhow::{Result, anyhow, bail, ensure};
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
@@ -55,10 +55,16 @@ struct Cli {
 fn main() -> Result<()> {
     let params = Cli::parse();
 
+    macro_rules! log {
+        ($($args:expr),*) => {
+            if !params.quiet {
+                eprintln!($($args),*);
+            }
+        };
+    }
+
     let bitrate_kbps = params.bitrate.unwrap_or_else(|| {
-        if !params.quiet {
-            eprintln!("Using default bitrate: {}kbps", DEFAULT_BITRATE);
-        }
+        log!("Using default bitrate: {}kbps", DEFAULT_BITRATE);
         DEFAULT_BITRATE
     });
     ensure!(bitrate_kbps >= 6.0, "bitrate {}kbps is too low", bitrate_kbps);
@@ -66,9 +72,7 @@ fn main() -> Result<()> {
     let bitrate = (bitrate_kbps * 1000.0).round() as _;
 
     let complexity = params.complexity.unwrap_or_else(|| {
-        if !params.quiet {
-            eprintln!("Using default complexity: {}", DEFAULT_COMPLEXITY);
-        }
+        log!("Using default complexity: {}", DEFAULT_COMPLEXITY);
         DEFAULT_COMPLEXITY
     });
     ensure!(matches!(complexity, 1..=10), "complexity should be in range 1-10");
@@ -85,22 +89,25 @@ fn main() -> Result<()> {
 
     let new_dimensions = params
         .dimensions
-        .map(|str| -> Result<_> {
-            let err = || anyhow!("dimensions should be in the format of \"WxH\"");
-            let (w, h) = str.split_once('x').ok_or_else(err)?;
-            Ok((w.parse().map_err(|_| err())?, h.parse().map_err(|_| err())?))
+        .map(|str| {
+            let (w, h) = str.split_once('x')?;
+            Some((w.parse().ok()?, h.parse().ok()?))
         })
+        .map(|opt| opt.ok_or_else(|| anyhow!("dimensions should be in the format of \"WxH\"")))
         .transpose()?;
 
     let quality = params.quality.unwrap_or_else(|| {
-        if !params.quiet && !matches!(target_format, ImageFormat::Copy) {
-            eprintln!("Using default image quality: {}", DEFAULT_QUALITY);
+        if !matches!(target_format, ImageFormat::Copy) {
+            log!("Using default image quality: {}", DEFAULT_QUALITY);
         }
         DEFAULT_QUALITY
     });
     ensure!(matches!(quality, 0.0..100.0), "quality value should be in range 0.0-100.0");
 
-    create_dir_all(&params.output)?;
+    if !params.output.exists() {
+        create_dir_all(&params.output)?;
+        log!("Created directory: {}", params.output.display());
+    }
 
     let files: Vec<_> = WalkDir::new(params.source)
         .into_iter()
@@ -117,112 +124,101 @@ fn main() -> Result<()> {
         .unwrap()
         .progress_chars(PROGRESS_BAR_CHARS);
     let progress_bar = ProgressBar::new(files.len() as _).with_style(progress_style);
-    progress_bar.inc(0);
+    progress_bar.inc(0); // show the bar
 
-    files
-        .into_par_iter()
-        .map(|path| -> Result<Option<(Error, _, _)>> {
-            macro_rules! unwrap {
-                ($val:expr, $new_path:expr) => {
-                    match $val {
-                        Ok(val) => val,
-                        Err(err) if params.skip_errors => {
-                            return Ok(Some((err.into(), path, $new_path)))
-                        }
-                        Err(err) => return Err(err.into()),
+    files.into_par_iter().try_for_each(|path| -> Result<()> {
+        macro_rules! unwrap {
+            ($result:expr) => {
+                match $result {
+                    Ok(val) => val,
+                    Err(err) if params.skip_errors => {
+                        progress_bar.suspend(|| {
+                            log!("Error when processing file: {}", path.display());
+                            log!("Error: {}", err);
+                        });
+                        progress_bar.inc(1);
+                        return Ok(());
                     }
-                };
-            }
-
-            let decoded = unwrap!(
-                if path.extension().unwrap() == "ncm" {
-                    DecodedData::from_ncm_file(&path)
-                } else {
-                    DecodedData::from_file(&path)
-                },
-                None
-            );
-
-            let new_filename = Path::new(path.file_name().unwrap()).with_extension("opus");
-            let mut new_path = params.output.join(&new_filename);
-            let mut overwritten_or_filename_altered = false;
-            let new_file = if params.overwrite {
-                if new_path.exists() {
-                    overwritten_or_filename_altered = true;
-                }
-                unwrap!(File::create(&new_path), None)
-            } else {
-                let mut new_stem = new_filename.file_stem().unwrap().to_string_lossy().into_owned();
-                loop {
-                    match OpenOptions::new().write(true).create_new(true).open(&new_path) {
-                        Ok(file) => break file,
-                        Err(e) if e.kind() == io::ErrorKind::AlreadyExists => {
-                            // add a number prefix to the filename stem
-                            // for example, "thing" renamed to "thing (1)" and "thing (41)" to "thing (42)"
-                            if let Some(left) = new_stem.strip_suffix(')')
-                                && let Some((stem, num_str)) = left.rsplit_once('(')
-                                && let Ok(n) = num_str.parse::<usize>()
-                            {
-                                new_stem = format!("{stem}({})", n + 1);
-                            } else {
-                                new_stem = format!("{new_stem} (1)");
-                            }
-                            new_path.set_file_name(&new_stem);
-                            new_path.add_extension(".opus");
-                            overwritten_or_filename_altered = true;
-                        }
-                        Err(e) => unwrap!(Err(e), None),
-                    }
+                    Err(err) => return Err(err.into()),
                 }
             };
+        }
 
-            let mut encoder = unwrap!(
-                OpusOggEncoder::new(
-                    decoded,
-                    bitrate,
-                    complexity,
-                    &mut ImageProcessor { target_format, new_dimensions, quality },
-                    new_file,
-                ),
-                Some(new_path)
-            );
+        let decoded = unwrap!(if path.extension().unwrap() == "ncm" {
+            DecodedData::from_ncm_file(&path)
+        } else {
+            DecodedData::from_file(&path)
+        });
 
-            while unwrap!(encoder.write_packet(), Some(new_path)) {}
+        let filename = Path::new(path.file_name().unwrap());
+        let mut new_path = params.output.join(filename);
+        new_path.set_extension("opus");
 
-            if overwritten_or_filename_altered {
-                let og_filename = path.file_name().unwrap().display();
-                let new_filename = new_path.file_name().unwrap().display();
+        let mut overwritten_or_filename_altered = false;
+        let new_file = if params.overwrite {
+            overwritten_or_filename_altered = new_path.exists();
+            unwrap!(File::create(&new_path))
+        } else {
+            let mut new_stem = filename.file_stem().unwrap().to_string_lossy().into_owned();
+            loop {
+                match OpenOptions::new().write(true).create_new(true).open(&new_path) {
+                    Ok(file) => break file,
+                    Err(e) if e.kind() == io::ErrorKind::AlreadyExists => {
+                        // add a number prefix to the filename stem
+                        // for example, "thing" renamed to "thing (1)" and "thing (41)" to "thing (42)"
+                        new_stem = if let Some(left) = new_stem.strip_suffix(')')
+                            && let Some((stem, num_str)) = left.rsplit_once(" (")
+                            && let Ok(n) = num_str.parse::<usize>()
+                        {
+                            format!("{stem} ({})", n + 1)
+                        } else {
+                            format!("{new_stem} (1)")
+                        };
+                        new_path.set_file_name(&new_stem);
+                        new_path.add_extension("opus");
+                        overwritten_or_filename_altered = true;
+                    }
+                    Err(e) => unwrap!(Err(e)),
+                }
+            }
+        };
+
+        macro_rules! unwrap_clean {
+            ($result:expr) => {{
+                let result = $result;
+                if result.is_err() {
+                    let _ = remove_file(&new_path);
+                }
+                unwrap!(result)
+            }};
+        }
+
+        let mut encoder = unwrap_clean!(OpusOggEncoder::new(
+            decoded,
+            bitrate,
+            complexity,
+            &mut ImageProcessor { target_format, new_dimensions, quality },
+            new_file,
+        ));
+
+        while unwrap_clean!(encoder.write_packet()) {}
+
+        if overwritten_or_filename_altered {
+            let og_filename = path.file_name().unwrap().display();
+            let new_filename = new_path.file_name().unwrap().display();
+            progress_bar.suspend(|| {
                 if params.overwrite {
-                    progress_bar.suspend(|| {
-                        println!("{} saved as and overwrote {}", og_filename, new_filename);
-                    });
+                    log!("{} saved as and overwrote {}", og_filename, new_filename);
                 } else {
-                    progress_bar.suspend(|| {
-                        println!("{} saved as {}", og_filename, new_filename);
-                    });
+                    log!("{} saved as {}", og_filename, new_filename);
                 }
-            }
+            });
+        }
 
-            Ok(None)
-        })
-        .try_for_each(|result| {
-            progress_bar.inc(1);
-            if let Ok(Some((err, path, new_path))) = &result {
-                if let Some(path) = new_path {
-                    remove_file(path)?;
-                }
+        progress_bar.inc(1);
+        Ok(())
+    })?;
 
-                if !params.quiet {
-                    progress_bar.suspend(|| {
-                        eprintln!("Error when processing file: {}", path.display());
-                        eprintln!("Error: {}", err);
-                    });
-                }
-            }
-            result.map(|_| ())
-        })?;
-
-    progress_bar.finish_and_clear();
-
+    progress_bar.finish();
     Ok(())
 }
